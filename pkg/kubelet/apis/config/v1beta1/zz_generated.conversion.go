43b1281b3534cc301ebd2014e3e357048dd4d3b0
Artyom Lukianov <alukiano@redhat.com>
2020-09-09 14:07:09 +0300

    Rename entities according to KEP
    As part of the PR we also need to fix klog import after the rebase.
    
    Signed-off-by: Artyom Lukianov <alukiano@redhat.com>

--- pkg/kubelet/apis/config/v1beta1/zz_generated.conversion.go
+++ pkg/kubelet/apis/config/v1beta1/zz_generated.conversion.go
@@ -350,7 +350,7 @@ func autoConvert_v1beta1_KubeletConfiguration_To_config_KubeletConfiguration(in 
 	if err := v1.Convert_Pointer_bool_To_bool(&in.EnableSystemLogHandler, &out.EnableSystemLogHandler, s); err != nil {
 		return err
 	}
-	out.PreReservedMemoryZone = *(*[]map[string]string)(unsafe.Pointer(&in.PreReservedMemoryZone))
+	out.ReservedMemory = *(*[]map[string]string)(unsafe.Pointer(&in.ReservedMemory))
 	return nil
 }
 
@@ -502,7 +502,7 @@ func autoConvert_config_KubeletConfiguration_To_v1beta1_KubeletConfiguration(in 
 	if err := v1.Convert_bool_To_Pointer_bool(&in.EnableSystemLogHandler, &out.EnableSystemLogHandler, s); err != nil {
 		return err
 	}
-	out.PreReservedMemoryZone = *(*[]map[string]string)(unsafe.Pointer(&in.PreReservedMemoryZone))
+	out.ReservedMemory = *(*[]map[string]string)(unsafe.Pointer(&in.ReservedMemory))
 	return nil
 }
 
